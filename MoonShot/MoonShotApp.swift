3a09a1f20a83350a51d872be8a59dd3d141e04e0
AndresGutierrez <c0dingw.dre@gmail.com>
2022-04-02 17:29:04 -0700

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Andres Gutierrez on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
